8dc36b9040b5b27230c6c902a71b8f3604f40ec2
augustobor <augus_borromeo@hotmail.com>
2023-02-06 12:30:45 -0300

    searchBar componente did it

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+// react imports
+import React, { useState, useRef } from 'react'
+
+// styles import
+import style from '../styles/css/searchBar.module.css'
 
 const searchBar = () => {
-  return (null)
+  const [chatSearch, setChatSearch] = useState('')
+
+  const searchInput = useRef(null)
+  const handleSearch = () => {
+    setChatSearch(searchInput.current.value)
+  }
+
+  return (
+    <section className={style.searchBar}>
+      <input
+        type='text'
+        placeholder='Busca chats aqui'
+        value={chatSearch}
+        ref={searchInput}
+        onChange={handleSearch}
+      />
+    </section>
+  )
 }
 
 export default searchBar
